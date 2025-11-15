48d8c81b3021f18dc8b072129ff9ce864624e9d5
SakshixSingh <sakshisingh020405@gmail.com>
2025-11-15 12:11:24 +0530

    url handling

--- index.js
+++ index.js
@@ -1,18 +1,51 @@
-// a simple web server
+const http = require("http");
+const fs = require("fs");
+const url = require("url");
 
-//http built-in module
-const http=require("http");
+const myServer = http.createServer((req , res)=>{
 
-//file system module 
-const fs=require("fs");
+    if(req.url === '/favicon.ico') return res.end();
 
-const myserver=http.createServer((req,res)=>{
-       const log=`${Date.now()}: New request recieved! \n`;
-       fs.appendFile("./log.txt",log,(err,dta)=>{
-        res.end("hello from server again")
-       })
-      console.log(req.headers);
-      res.end("hello from server");
+
+    const log = `${Date.now()} : ${req.url} : New Request Recieved \n`;
+    const myUrl = url.parse(req.url , true);
+    console.log(myUrl);
+
+    fs.appendFile('log.txt' , log , (err , data)=>{
+
+        switch(myUrl.pathname){
+            case '/' : res.end("Home Page");
+            break;
+
+            case '/about' : 
+            //query parameter
+            const username = myUrl.query.myname ;
+            console.log(username)
+            res.end(`Hii,  ${username}`);
+            break;
+
+            case "/search":
+                const search = myUrl.query.search_query;
+                console.log(search)
+                res.end("Here are your result for "+search);
+                break;
+
+            default:
+                res.end("404 Not Found")
+
+        }
+        
+        
+    })
+
+
+    // console.log(req.headers)
+    // console.log(req)
+    
+
 });
 
-myserver.listen(8000,()=>{console.log("Hello from server! ")});
+const PORT = 8000;
+myServer.listen(8000, ()=>{
+    console.log(`Server started at port no ${PORT} `);
+})
